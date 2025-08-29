587560ae42a37f4f8b468cc575cd50bf9ceddd7a
ckrb72 <krck70153@gmail.com>
2025-08-29 00:12:53 -0400

    first square rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <fstream>
+#include <string>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+bool load_shader(const std::string& vertex_path, const std::string& fragment_path, unsigned int* program_ptr);
 
 void glfw_error_fun(int error, const char* err_desc)
 {
     std::cout << err_desc << std::endl;
 }
+
+const int WIN_WIDTH = 1920;
+const int WIN_HEIGHT = 1080;
 
 int main()
 {
@@ -17,7 +27,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* window = glfwCreateWindow(1920, 1080, "Physics Sim", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "Physics Sim", nullptr, nullptr);
 
     if(!window)
     {
@@ -41,11 +51,64 @@ int main()
 
     glClearColor(0.3, 0.3, 0.3, 1.0);
 
+    unsigned int vao, vbo, ebo;
+
+
+    float vertices[] = 
+    {
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0,
+        0.5, 0.5, 0.0,
+        -0.5, 0.5, 0.0
+    };
+
+    unsigned int indices[] = 
+    {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &ebo);
+    glGenBuffers(1, &vbo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    unsigned int program;
+
+    if(!load_shader("../shader/default.vert", "../shader/default.frag", &program))
+    {
+        std::cout << "Failed to load shader" << std::endl;
+
+        exit(EXIT_FAILURE);
+    }
+
+    glm::mat4 perspective = glm::perspective(glm::radians(90.0f), (float)WIN_WIDTH / (float)WIN_HEIGHT, 0.1f, 10.0f);
+
+    glUniformMatrix4fv(glGetUniformLocation(program, "projection"), 1, GL_FALSE, glm::value_ptr(perspective));
+
     while(!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(program);
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         glfwSwapBuffers(window);
     }
@@ -53,4 +116,102 @@ int main()
 
     glfwTerminate();
     std::cout << "Hello World" << std::endl;
+}
+
+std::string read_file(const std::string& path)
+{
+    std::ifstream file;
+    file.open(path, std::ios::in);
+
+    if(!file.is_open())
+    {
+        std::cout << "failed to open file" << std::endl;
+
+        return "";
+    }
+
+    std::string str;
+    std::string line;
+
+    while(std::getline(file, line))
+    {
+        str += line + "\n";
+    }
+
+    file.close();
+
+    return str;
+}
+
+bool load_shader(const std::string& vertex_path, const std::string& fragment_path, unsigned int* program_ptr)
+{
+    const int MAX_LOG = 512;
+    int success;
+    char log[MAX_LOG];
+
+    std::string vertex_str = read_file(vertex_path);
+
+    const char* const vertex_src = vertex_str.c_str();
+
+    unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_src, nullptr);
+    glCompileShader(vertex_shader);
+
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertex_shader, MAX_LOG, nullptr, log);
+        std::cerr << "Vertex: " << std::endl;
+        std::cerr << log << std::endl;
+        glDeleteShader(vertex_shader);
+        return false;
+    }
+
+    std::string fragment_str = read_file(fragment_path);
+
+    const char* const fragment_src = fragment_str.c_str();
+
+    unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_src, nullptr);
+    glCompileShader(fragment_shader);
+
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragment_shader, MAX_LOG, nullptr, log);
+        std::cerr << "Fragment: " << std::endl;
+        std::cerr << log << std::endl;
+        glDeleteShader(vertex_shader);
+        glDeleteShader(fragment_shader);
+        return false;
+    }
+
+    unsigned int program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetProgramInfoLog(program, MAX_LOG, nullptr, log);
+        std::cerr << "Linker: " << std::endl;
+        std::cerr << log << std::endl;
+        glDetachShader(program, vertex_shader);
+        glDetachShader(program, fragment_shader);
+        glDeleteShader(vertex_shader);
+        glDeleteShader(fragment_shader);
+        glDeleteProgram(program);
+        return false;
+    }
+
+    glDetachShader(program, vertex_shader);
+    glDetachShader(program, fragment_shader);
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    *program_ptr = program;
+
+    return true;
 }
